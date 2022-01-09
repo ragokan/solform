8fa9f16015faf0d2fab14241e43569cb8771830c
ragokan <okanyldrmop@gmail.com>
2022-01-09 18:26:56 +0300

    remove internal types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,13 @@
-/** @internal */
 export type SolFormError<Values> = { [key in keyof Values]?: string };
 
 export type SolFormValidator<T> = (value: T) => string | void;
 
-/** @internal */
 export type SolFormElement = HTMLInputElement | HTMLSelectElement;
 
-/** @internal */
 export type SolFormValidatorsOption<Values> = {
   [key in keyof Partial<Values>]: SolFormValidator<Values[key]> | Array<SolFormValidator<Values[key]>>;
 };
 
-/** @internal */
 export interface SolFormOptions<Values> {
   initialValues?: Partial<Values>;
   validators?: SolFormValidatorsOption<Values>;
